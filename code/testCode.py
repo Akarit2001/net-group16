5792d308fd55b994824d26d94c8c1cbb61d4e6b0
IFEELS3UPID <akarit2001@gmail.com>
2022-03-22 02:27:46 +0700

    แก้คอมเม้นนิดหน่อย

--- code/testCode.py
+++ code/testCode.py
@@ -4,20 +4,10 @@ import datetime
 ## การจะใช้คำสั่งใน mysqls ได้ต้องสร้าง database ให้เหมือนในโปรเจคนี้ก่อน ตั้งชื่อ database ว่า foodservice และมี   ##
 ## table เหมือนใน file net-group16\code\pysql\create_table_and_table_struct\createTable.py         ##
 ####################################################################################################
-# ms.MySql().createUser(username='user234',password='user23', address='KKU')
-# print('--------------------------------------')
-# ms.MySql().showAllUser()
-# print('--------------------------------------')
-# ms.MySql().showUser(UIDs="00000000000",username='admin')
-# print('--------------------------------------')
-# # ms.MySql().dropUser(UIDs='08281117706', username='user2')
-# print('--------------------------------------')
+
 print(ms.MySql().getAllUser())
 
-# ss = ms.MySql().getUser(username='user23s')
-# # print(ss,type(ss), not ss)
-# for s in ss:
-#     print(s,type(s))
+
 sql = ms.MySql()
 # print('--------------------------------------------------')
 # ms.MySql().addFoods("somtum", 50)
@@ -32,11 +22,6 @@ print(type(sql.getFoods('somtum')[0]))
 date = datetime.datetime.now()
 print(date.strftime("%Y-%m-%d"))
 
-# user1 = ms.MySql()
-# for i in range(3):
-#     fids = str(input())
-#     totals = int(input())
-#     user1.genBill(fids, '07055401949', totals)
 print("--------------------getbillUserDetail-----------------------")
 strs = ms.MySql().getbillUserDetail(userId="07055401949")
 print(strs)
@@ -45,8 +30,8 @@ print(str1)
 print("--------------------showAllBill-----------------------")
 print(ms.MySql().getAllBill())
 
-  #############################################
- ##          genbillเมื่อลูกค้าสั่ง oder          ##
+#############################################
+##          genbillเมื่อลูกค้าสั่ง oder          ##
 #############################################
 # user2 = ms.MySql()
 # user2.genBill('020855', '07055401949', 10)
@@ -60,13 +45,16 @@ ms.MySql().getbillUserDetail(userId="07055401949")
 print("-------------------------------------------")
 
 ###################################################
+
+
 def startClient():
-  print('='*15 + '  Welcome to Foodservice  ' + '='*15+'\n')
-  
+    print('='*15 + '  Welcome to Foodservice  ' + '='*15+'\n')
+
+
 def checkChoice(choice):
-  print("\t1. Login\n\t2. Register\n\t3. Bill history")
-  choice = int("Select : ")
-  if choice == '1':
+    print("\t1. Login\n\t2. Register\n\t3. Bill history")
+    choice = int("Select : ")
+    if choice == '1':
         print('Welcome You are going to login')
         login()
 #         send(choice)
@@ -79,23 +67,22 @@ def checkChoice(choice):
         ######
     else:
         print('\t\t' + '!'*5 + ' Invalid option please try again ' + '!'*5 + '\n')
-        
+
+
 def login():
     username = input('Username : ')
     password = input('Password : ')
     ####
     ####
     print('You are logged!\n')
-    
+
+
 def register():
     username = input('Username : ')
     password = input('Password : ')
     address = input('Address : ')
     ####
-    if :
-      ms.Mysql.createUser(username,password, address)
-    else:
-       ######
-   
-    print('You are registered!\n')
+    ms.Mysql.createUser(username, password, address)
+    ######
 
+    print('You are registered!\n')
